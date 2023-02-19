c3497318b16f604dc4e77b6e338655aeae11b9fa
eddyangelop <eddysoullive@gmail.com>
2023-02-19 09:48:21 -0300

    criado primeiro controlador (produtosController) da api

---
+++ ApiCatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,19 @@
+﻿using ApiCatalogo.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
