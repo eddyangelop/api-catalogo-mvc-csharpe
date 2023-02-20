d8771d804615cafb0a8ba24c175fb5ae03cb94b7
eddyangelop <eddysoullive@gmail.com>
2023-02-20 08:12:05 -0300

    criando o método Post

--- ApiCatalogo/Controllers/ProdutosController.cs
+++ ApiCatalogo/Controllers/ProdutosController.cs
@@ -20,22 +20,37 @@ namespace ApiCatalogo.Controllers
         public ActionResult<IEnumerable<Produto>> Get()
         {
             var produtos = _context.Produtos.ToList();
-            if(produtos is null)
+            if (produtos is null)
             {
                 return NotFound("Produtos não encontrados...");
             }
             return produtos;
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name = "ObterProduto")]
         public ActionResult<Produto> Get(int id)
         {
             var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
-            if(produto is null)
+            if (produto is null)
             {
                 return NotFound("Produto não encontrado...");
             }
             return produto;
         }
+
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+            if (produto is null)
+            return BadRequest();
+            
+
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterProduto",
+                new { id = produto.ProdutoId }, produto);
+        }
+
     }
 }
